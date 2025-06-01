3d0366dcc13c19b778bd321aaabc033db65d9ebc
Christopher Boyle <christopher.boyle.101@strath.ac.uk>
2025-06-01 21:00:18 +0100

    Add noises, effects

--- src/patch.rs
+++ src/patch.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::OpenOptions};
+use std::{collections::HashMap, fs::OpenOptions, sync::Arc};
 
 use serde::{Serialize, Deserialize};
 
@@ -8,29 +8,88 @@ use fundsp::hacker::*;
 #[serde(tag="op")]
 pub enum PatchNode {
     Constant { c: f32 },
+
+    // Oscillators
     Sine,
     Saw,
     Square,
     SpecifiedSine { freq: f32 },
     SpecifiedSaw { freq: f32 },
     SpecifiedSquare { freq: f32 },
+
+    // Sample
+    Sample { path: String },
+
+    // Noise
+    WhiteNoise,
+    PinkNoise,
+    BrownNoise,
+
+    // Effects
+    FlangerSin { strength: f32, min_delay: f32, max_delay: f32, sin_freq: f32 },
+    ADSR { attack: f32, decay: f32, sustain: f32, release: f32 },
+
+    // Filters
+    // TODO
+
+    // Maths
+    SumChannels,
+    MultChannels,
     Mux,
-    Pan { balance: f32 },
+
+    // Pan { balance: f32 },
 }
 
 impl PatchNode {
-    pub fn add_to_net(&self, net: &mut Net) -> NodeId {
-        match self {
+    pub fn add_to_net(&self, net: &mut Net) -> anyhow::Result<NodeId> {
+        let rv = match self {
             Self::Constant { c }            => { net.push(Box::new(constant(*c))) },
+
+            // Oscillators
             Self::Sine                      => { net.push(Box::new(sine())) },
             Self::Saw                       => { net.push(Box::new(saw())) },
             Self::Square                    => { net.push(Box::new(square())) },
             Self::SpecifiedSine { freq }    => { net.push(Box::new(sine_hz(*freq))) },
             Self::SpecifiedSaw { freq }     => { net.push(Box::new(saw_hz(*freq))) },
             Self::SpecifiedSquare { freq }  => { net.push(Box::new(square_hz(*freq))) },
-            Self::Mux                       => { net.push(Box::new( map(|f: &Frame<f32, U2>| f[0]*f[1]) )) }
-            Self::Pan { balance }           => { net.push(Box::new(pan(*balance))) },
-        }
+
+            // Sample
+            Self::Sample { path }           => {
+                let wave = Wave::load(path)?;
+                let wave = Arc::new(wave);
+                net.push(Box::new(
+                    wavech(&wave, 0, Some(0))
+                ))
+            }
+
+            // Maths
+            Self::SumChannels               => { net.push(Box::new( map(|f: &Frame<f32, U2>| f[0]+f[1]) )) },
+            Self::MultChannels | Self::Mux  => { net.push(Box::new( map(|f: &Frame<f32, U2>| f[0]*f[1]) )) },
+
+            // Noise
+            Self::WhiteNoise                => { net.push(Box::new( white() )) },
+            Self::PinkNoise                 => { net.push(Box::new( pink() )) },
+            Self::BrownNoise                => { net.push(Box::new( brown() )) },
+
+            // Effects
+            Self::FlangerSin { strength, min_delay, max_delay, sin_freq } => { 
+                let strength = strength.clone(); let min_delay = min_delay.clone(); let max_delay = max_delay.clone(); let sin_freq = sin_freq.clone();
+                net.push(Box::new(
+                    flanger(strength, min_delay, max_delay, move |t| lerp11(min_delay, max_delay, sin_hz(sin_freq, t)))
+                ))
+            },
+            Self::ADSR { attack, decay, sustain, release } => {
+                net.push(Box::new( adsr_live(*attack, *decay, *sustain, *release) ))
+            }
+
+            // Filters
+            // TODO
+
+            // Self::Pan { balance }           => { net.push(Box::new(pan(*balance))) },
+            _ => { todo!() }
+        };
+
+        Ok(rv)
     }
 }
 
@@ -45,11 +104,15 @@ impl Patch {
     pub fn new() -> Self {
         let mut nodes = HashMap::new();
         nodes.insert("osc1".into(), PatchNode::Saw);
+        nodes.insert("flanger".into(), PatchNode::FlangerSin { strength: 0.5, min_delay: 0.005, max_delay: 0.01, sin_freq: 0.1 });
+        nodes.insert("adsr".into(), PatchNode::ADSR { attack: 1.0, decay: 0.5, sustain: 0.5, release: 0.5 });
         nodes.insert("mux".into(), PatchNode::Mux);
         let mut edges = Vec::new();
         edges.push(("freq".into(), "osc1".into()));
-        edges.push(("osc1".into(), "mux:0".into()));
-        edges.push(("ctl".into(), "mux:1".into()));
+        edges.push(("osc1".into(), "flanger".into()));
+        edges.push(("flanger".into(), "mux:0".into()));
+        edges.push(("ctl".into(), "adsr".into()));
+        edges.push(("adsr".into(), "mux:1".into()));
         edges.push(("mux".into(), "out".into()));
         Self { nodes, edges }
     }
@@ -101,7 +164,7 @@ impl Patch {
 
         let mut nodes_by_id = HashMap::new();
         for (node_name, node) in self.nodes.iter() {
-            let node_id = node.add_to_net(&mut net);
+            let node_id = node.add_to_net(&mut net)?;
             nodes_by_id.insert(node_name.clone(), node_id);
         }
 
